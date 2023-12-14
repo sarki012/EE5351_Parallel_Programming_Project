e5181f073cbbde230c1e216fe42482f7b070a948
sarki012 <60797267+sarki012@users.noreply.github.com>
2023-12-14 17:55:27 -0600

    Add files via upload

--- histogram/histogram.cpp
+++ histogram/histogram.cpp
@@ -11,17 +11,9 @@ using namespace std;
 #include <opencv2/core/core.hpp>
 #include <cuda_runtime.h>
 
-void convert_to_gray(const cv::Mat& input, cv::Mat& output);
+void histogram(const cv::Mat& input, cv::Mat& output);
  
 int main() {
-	clock_t start, end;
-
-	//time_t start, end;
-	//time(&start);
-        // unsync the I/O of C and C++. 
-   	 ///ios_base::sync_with_stdio(false); 
- 
-	
 	std::string imagePath = "snowboard.png";
 
 	// Read input image from the disk
@@ -36,22 +28,9 @@ int main() {
 	// Create output image
 	cv::Mat output(input.rows,input.cols,CV_8UC3);
 
-	start = clock();
 	// Call the wrapper function
-	convert_to_gray(input,output);
-	end = clock();
-	double time_taken = double(end - start) / double(CLOCKS_PER_SEC);
-    	cout << "Time taken by program is : " << fixed 
-         << time_taken << setprecision(10);
-    	cout << " sec " << endl;
-	/*
-	time(&end); 
-	    // Calculating total time taken by the program. 
-    	double time_taken = double(end - start); 
-   	 cout << "Time taken by program is : " << fixed 
-        << time_taken << setprecision(5); 
-   	 cout << " sec " << endl; 
-   	 */
+	histogram(input,output);
+
 	// Show the input and output
 	//cv::imshow("Input",input);
 	//cv::imshow("Output",output);
--- histogram/histogram_kernel.cu
+++ histogram/histogram_kernel.cu
@@ -1,4 +1,4 @@
-/* Vector Addition: C = A + B.
+/* Image Histogram .cu
  * Device code.
  */
 
@@ -6,17 +6,18 @@
  #include "histogram.h"
  
  #include <cuda.h>
- 
+ using namespace std; 
  
 #include <iostream>
 #include <cstdio>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <cuda_runtime.h>
+#include <iomanip>
 
-void convert_to_gray(const cv::Mat& input, cv::Mat& output);
+void histogram(const cv::Mat& input, cv::Mat& output);
 
-__global__ void bgr_to_gray_kernel(unsigned char* input, unsigned char* output, int width, int height, int colorWidthStep, int grayWidthStep, float *bins_d) {
+__global__ void histogram_kernel(unsigned char* input, unsigned char* output, int width, int height, int colorWidthStep, int grayWidthStep, unsigned int *bins_d) {
     int bx = blockIdx.x;
     int tx = threadIdx.x;
     int bdimx = blockDim.x;
@@ -34,33 +35,34 @@ __global__ void bgr_to_gray_kernel(unsigned char* input, unsigned char* output, 
     if((xIndex<width) && (yIndex<height)){
 
         int value = input[color_tid];		//Blue first
-        atomicAdd(&bins_d[value], 0.01);
-        if(bins_d[value] > 255){
-            bins_d[value] = 255;
-        }
+        atomicAdd(&bins_d[value], 1);
     }
     output[out_tid] = input[color_tid];
 }
  
  ////////////////////////////////////////////////////////////////////////////////
-void convert_to_gray(const cv::Mat& input, cv::Mat& output) {
+void histogram(const cv::Mat& input, cv::Mat& output) {
+	cudaEvent_t start, stop;
+	cudaEventCreate(&start);
+	cudaEventCreate(&stop);
+
 	// Calculate total number of bytes of input and output image
 	const int colorBytes = input.step * input.rows;
 	const int grayBytes = output.step * output.rows;
 
 	unsigned char *input_d, *output_d;
-	float *bins_d;
-	float *kernel_bins;
+	unsigned int *bins_d;
+	unsigned int *kernel_bins;
 	
-	kernel_bins = (float *)malloc(256*sizeof(float));
+	kernel_bins = (unsigned int *)malloc(256*sizeof(unsigned int));
 
 	// Allocate device memory
 	cudaMalloc<unsigned char>(&input_d,colorBytes);
 	cudaMalloc<unsigned char>(&output_d,grayBytes);
 	
-	cudaMalloc<float>(&bins_d,256*sizeof(float));
+	cudaMalloc<unsigned int>(&bins_d,256*sizeof(unsigned int));
 	//SAFE_CALL(cudaMalloc(void**)(&bins_d,256*sizeof(int),"CUDA Malloc Failed");
-	cudaMemset(bins_d, 0, 256*sizeof(float));
+	cudaMemset(bins_d, 0, 256*sizeof(unsigned int));
 
 	// Copy data from OpenCV input image to device memory
 	cudaMemcpy(input_d,input.ptr(),colorBytes,cudaMemcpyHostToDevice);
@@ -71,17 +73,29 @@ void convert_to_gray(const cv::Mat& input, cv::Mat& output) {
 	// Calculate grid size to cover the whole image
 	const dim3 grid((input.cols + block.x - 1)/block.x, (input.rows + block.y - 1)/block.y);
 
+	cudaEventRecord(start);
 	// Launch the color conversion kernel
-	bgr_to_gray_kernel<<<grid,block>>>(input_d,output_d,input.cols,input.rows,input.step,output.step, bins_d);
+	histogram_kernel<<<grid,block>>>(input_d,output_d,input.cols,input.rows,input.step,output.step, bins_d);
 	
-	cudaMemcpy(kernel_bins, bins_d, 256*sizeof(float), cudaMemcpyDeviceToHost);
+	cudaEventRecord(stop);
+	
+	cudaEventSynchronize(stop);
+	float milliseconds = 0;
+	cudaEventElapsedTime(&milliseconds, start, stop);
+
+    
+	
+	cudaMemcpy(kernel_bins, bins_d, 256*sizeof(unsigned int), cudaMemcpyDeviceToHost);
 	//Synchronie host and device to ensure that transfer is finished
 	cudaDeviceSynchronize();
 	
 	
 	for(int i = 0; i < 255; i++){
-		printf(" %.1f", kernel_bins[i]);
+		printf(" %u", kernel_bins[i]);
 	}
+	
+	cout << "Time taken by program is : "<< fixed << setprecision(10) << milliseconds;
+    	cout << " milliseconds " << endl;
 	
 	
     	cudaFree(input_d);
